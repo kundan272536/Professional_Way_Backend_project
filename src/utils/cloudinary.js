2c91f95bf3d8e618074d9d44131daea16584636c
KUNDAN KUMAR <103889218+kundan272536@users.noreply.github.com>
2024-04-28 18:21:55 +0530

    File Upload and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";  
+cloudinary.config({ 
+  cloud_name: 'dklycn5tw', 
+  api_key: '782454938374929', 
+  api_secret: '30a0D6x-V4lpnnvZyRIcYt7yBVE' 
+});
+
+
+const uploadOnCloudinary=async (localFilepath)=>{
+  try {
+    if(!localFilepath) return null;
+    //File Uploaded at cloudinary
+    const response=await cloudinary.uploader.upload(localFilepath,{
+        resource_type:"auto"
+    })
+    console.log("File is uploaded at cloudinary",response.url);
+    return response;
+  } catch (error) {
+     fs.unlinkSync(localFilepath);
+     // Remove the locally saved temporary file as the upload operation get failed
+
+
+     return null
+  }
+}
