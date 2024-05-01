9c283e57a0eee33a91c9645aa4b88e27578890d5
KUNDAN KUMAR <103889218+kundan272536@users.noreply.github.com>
2024-05-01 23:16:13 +0530

    Only added variable in registeruser and loginUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,11 +2,13 @@ import { asynHandlers } from "../utils/ayncHandlers.js";
 
 
 const registerUser=asynHandlers(async(req,res)=>{
+    const {fname,lname,email,contact_no}=req.body;
     res.status(200).json({
         message:"ok"
     })
 });
 const loginUser=asynHandlers(async(req,res)=>{
+    const {email, password}=req.body;
     res.status(200).json({
         message:"ok"
     })
